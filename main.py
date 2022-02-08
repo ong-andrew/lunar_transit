75e8fe450694de6001522bd126697530c7997a6c
Andrew34000 <79370960+ong-andrew@users.noreply.github.com>
2022-02-08 21:53:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ place.date = '2022/02/05'
 place.lat = '5.042251'
 place.lon = '100.665398'
 m = ephem.Moon()
-print(bm.next_transit(m))
-print(bm.next_rising(m))
+print(place.next_transit(m))
+print(place.next_rising(m))
